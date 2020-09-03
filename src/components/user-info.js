ba584e6b1282dd6cea55ea6d17e640562dadc708
FredHorizon <fredoliveira.sys@gmail.com>
2020-09-03 16:11:58 -0400

    Configura os estilos

--- src/components/user-info.js
+++ src/components/user-info.js
@@ -6,7 +6,7 @@ const UserInfo = () => (
   <div className='user-info'>
     <img src='https://avatars3.githubusercontent.com/u/30598272?v=4' alt=''/>
     <h1 className='username'>
-      <a href='https://github.com/FredHorizon'>Fred Oliveira</a>
+      <a href='https://github.com/FredHorizon' target='blank'>Fred Oliveira</a>
     </h1>
 
     <ul className='repos-info'>
